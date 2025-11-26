b0bcfd60c2b2f88130c52ba064d26bd0ec069548
Ahmed nasri <ahmednasriabc@gmail.com>
2025-11-26 13:40:43 +0100

    Client,server  file fully working :D

--- client/client.py
+++ client/client.py
@@ -1,5 +1,5 @@
 import socket
-
+import socket
 
 class ClientTasks:
     def __init__(self, host, port):
@@ -18,11 +18,51 @@ class ClientTasks:
     def close(self):
         self.client_socket.close()
 
+    def menu(self):
+        
+        while True:
+            print("\n_____________MENU_____________")
+            print("1 _ ADD TASK")
+            print("2 _ DELETE TASK")
+            print("3 _ LIST TASKS")
+            print("4 _ CHANGE STATUS")
+            print("5 _ EXIT")
+            cmd = input("Command: ").strip()
+
+            if cmd == "1": 
+                title = input("Task Title: ").strip()
+                desc = input("Task Description: ").strip()
+                auth = input("Task Author: ").strip()
+                command = f"ADD:{title}:{desc}:{auth}"
+                print(self.send_command(command))
+
+            elif cmd == "2":  
+                task_id = input("Task ID to delete: ").strip()
+                command = f"DEL:{task_id}"
+                print(self.send_command(command))
+
+            elif cmd == "3": 
+                print(self.send_command("LIST"))
+
+            elif cmd == "4":  
+                task_id = input("Task ID to change: ").strip()
+                new_stat = input("New Status (ToDo/Doing/Done): ").strip()
+                command = f"STAT:{task_id}:{new_stat}"
+                print(self.send_command(command))
+
+            elif cmd == "5":  
+                self.send_command("EXIT")
+                self.close()
+                print("Disconnected from server.")
+                break
+
+            else:
+                print("Invalid command! Choose 1-5.")
 
 
 
 
-print("HI CLIENT !")
+
 client = ClientTasks("server_cont", 12345)
 client.menu()
 
